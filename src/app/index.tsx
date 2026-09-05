5d195f3088419108744555e3aeb2f224b3189336
Julio Cesar De Castro Araujo <juliocssrr@gmail.com>
2026-09-04 21:22:00 -0300

    Atualização de texto

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Eu amo batatas</Text>
+      <Text>ABCDEFG</Text>
     </View>
   );
 }
